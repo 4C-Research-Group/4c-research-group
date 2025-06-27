e63bd12d64d5ab9872a7784b93a52be99b72e792
pranav-k-jha <pranav.jha@mail.concordia.ca>
2025-06-27 16:46:45 -0400

    updated

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -218,12 +218,6 @@ export default function PagesLayout({
                 <div className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black/5 dark:ring-white/10 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-50">
                   <div className="py-1">
                     <Link
-                      href="/research"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700"
-                    >
-                      Research
-                    </Link>
-                    <Link
                       href="/knowledge-mobilization"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700"
                     >
