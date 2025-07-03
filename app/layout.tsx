f4718099a150d796909a0b53201be631b006a367
pranav-k-jha <pranav.jha@mail.concordia.ca>
2025-07-02 22:03:25 -0400

    Change default theme to light mode instead of system preference

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
           <AuthProvider>
             <ThemeProvider
               attribute="class"
-              defaultTheme="system"
+              defaultTheme="light"
               enableSystem
               disableTransitionOnChange
             >
