90b6273062c298f6f170e7ed983e166da2cef122
pranav-k-jha <pranav.jha@mail.concordia.ca>
2025-07-07 19:20:11 -0400

    update contact

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -37,7 +37,7 @@ const DEFAULT_CONTACT: ContactPage = {
   hours: "Monday–Friday: 9:00 AM–5:00 PM, Saturday–Sunday: Closed", // Remove spaces around en dash in time range
   hero_title: "Get In Touch",
   hero_description:
-    "Let us know if you are interested in learning more about our research, collaborating with our team, or contributing to our mission. If you are a student looking for opportunities to participate in research, please do not hesitate to reach out!",
+    "Let us know if you are interested in learning more about our research, collaborating with our team, or contributing to our mission.",
   map_embed_url:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5979841.431727101!2d-90.98107327499999!3d42.960482299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882ef0fa90d42453%3A0x1e8dae5de3acaae!2sVictoria%20Hospital%20%26%20Children's%20Hospital!5e0!3m2!1sen!2sca!4v1751160990375!5m2!1sen!2sca",
   updated_at: "",
